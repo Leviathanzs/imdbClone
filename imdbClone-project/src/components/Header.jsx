3e4bd4b6e0cff6e595c8ff9c5a9066b91f68e59a
Leviathanzs <okywidhiansyah@gmail.com>
2023-08-18 20:33:31 +0700

    Working on header section

---
+++ imdbClone-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header () {
+    return (
+        <div className="navbar">
+            <a href="#" className="navbar-logo">OMDB<span>FILM</span></a>
+
+            <div className="navbar-nav">
+                <a href="#">Home</a>
+                <a href="#">Film</a>
+            </div>
+        </div>
+    )
+}
