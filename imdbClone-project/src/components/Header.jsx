9184b0a0dbcb8f0540846ff746038c3dd1c91b03
Leviathanzs <okywidhiansyah@gmail.com>
2023-08-18 22:38:42 +0700

    Half done header and hero section

--- imdbClone-project/src/components/Header.jsx
+++ imdbClone-project/src/components/Header.jsx
@@ -1,11 +1,11 @@
 export default function Header () {
     return (
         <div className="navbar">
-            <a href="#" className="navbar-logo">OMDB<span>FILM</span></a>
+            <a href="#" className="navbar-logo">OMDB<span>MOVIE</span></a>
 
             <div className="navbar-nav">
                 <a href="#" className="active">Home</a>
-                <a href="#">Film</a>
+                <a href="#">Movie</a>
             </div>
         </div>
     )
--- imdbClone-project/src/components/Hero.jsx
+++ imdbClone-project/src/components/Hero.jsx
@@ -2,9 +2,16 @@ export default function Hero () {
     return (
         <section id="home" className="hero">
             <main className="content">
-                <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, adipisci.</h1>
-                <p>Lorem ipsum dolor sit amet.</p>
-                <a href="#" className="btn">Movie</a>
+                <div className="main-content">
+                    <h1>the best place to find the best movie</h1>
+                    <p>OMDb is the world's most popular and authoritative source for movie, 
+                        TV and celebrity content. Find ratings and reviews for the newest movie and 
+                        TV shows.</p>
+                    <a href="#" className="btn">Movie</a>
+                </div>
+                <div className="content-image">
+                    <img src="img/mix.jpg" alt="mix movie" />
+                </div>
             </main>
         </section>
     )
